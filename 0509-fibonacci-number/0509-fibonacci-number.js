bbd1c44793bacbad6ce3383fe51706917460463c
Ellie Aghajani <ellie@northarrowresearch.com>
2026-07-06 07:48:22 -0700

    Time: 44 ms (75.29%), Space: 54.1 MB (25.49%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ next = current + previous
+ */
+var fib = function(n) {
+    if (n <= 1) return n;
+    let prev = 0;
+    let current = 1
+    for (let i = 2; i <= n; i++){
+        const next = prev + current;
+        prev = current;
+        current = next;
+    }
+    return current
+};
