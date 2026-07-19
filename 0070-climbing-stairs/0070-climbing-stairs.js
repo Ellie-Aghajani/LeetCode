c2bd5a4fa78956e07cc41f0eaa6768b1f694b1e3
Ellie Aghajani <ellie@northarrowresearch.com>
2026-07-19 15:05:26 -0700

    Time: 0 ms (100%), Space: 53.5 MB (53.88%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -2,14 +2,17 @@
  * @param {number} n
  * @return {number}
  */
-var climbStairs = function(n) {
+var climbStairs = function(n, memo = {}) {
     if (n <= 2) return n;
-    let prev = 1;
-    let current = 2;
-    for (let i = 3; i <= n; i++){
-        const next = prev + current;
-        prev = current;
-        current = next;
-    }
-    return current;
+    if (n in memo) return memo[n];
+    memo[n] = climbStairs(n-1, memo) + climbStairs(n-2, memo)
+    return memo[n];
+    // let prev = 1;
+    // let current = 2;
+    // for (let i = 3; i <= n; i++){
+    //     const next = prev + current;
+    //     prev = current;
+    //     current = next;
+    // }
+    // return current;
 };
