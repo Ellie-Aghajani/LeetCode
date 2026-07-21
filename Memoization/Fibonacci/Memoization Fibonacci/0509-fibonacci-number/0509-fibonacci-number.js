3f2dad5deb7b0c23da831adaa57547b7cbf97784
Ellie-Aghajani <z.aqajani@gmail.com>
2026-07-21 09:27:13 -0700

    update directory

--- Memoization/Fibonacci/Memoization Fibonacci/0509-fibonacci-number/0509-fibonacci-number.js
+++ Memoization/Fibonacci/Memoization Fibonacci/0509-fibonacci-number/0509-fibonacci-number.js
@@ -3,12 +3,29 @@
  * @return {number}
  next = current + previous
  */
-var fib = function(n, memo={}) {
-    if(n<=1) return n;
-    memo[n] = fib(n-2, memo) + fib(n-1, memo);
-    return memo[n]
-}
+// var fib = function(n, memo={}) {
+//     if(n<=1) return n;
+//     memo[n] = fib(n-1, memo) + fib(n-2, memo); //drill down to base case
+//     return memo[n]
+// }
 
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+
+  const memo = { 0: 0, 1: 1 };
+
+  function helper(n) {
+    if (n in memo) return memo[n];
+
+    memo[n] = helper(n - 1) + helper(n - 2);
+    return memo[n];
+  }
+
+  return helper(n);
+};
 
 
 // var fib = function(n) {
